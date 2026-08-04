3be6a550d2b0bf682df38a6ae99bbc6a306de9fc
NagaLakshmi477 <jaishikareddy997@gmail.com>
2026-08-04 09:34:23 +0530

    k8s

--- 80-eks/main.tf
+++ 80-eks/main.tf
@@ -49,19 +49,19 @@ module "eks" {
     #     AmazonEKSLoad = "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy"
     # }
     green = {
-      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
-      ami_type       = "AL2023_x86_64_STANDARD" # user name is ec2-user
-      instance_types = ["m5.xlarge"]
-      
-      min_size     = 2
-      max_size     = 10
-      desired_size = 2
+ami_type = "AL2023_x86_64_STANDARD"
+instance_types = ["m7i-flex.large"]
 
-      iam_role_additional_policies = {
-        AmazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-        AmazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
-        AmazonEKSLoad = "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy"
-      }
+min_size = 2
+max_size = 10
+desired_size = 2
+
+iam_role_additional_policies = {
+AmazonEBS = "arn:aws:iam::aws/service-role/AmazonEBSCSIDriverPolicy"
+AmazonEFS = "arn:aws:iam::aws/service-role/AmazonEFSCSIDriverPolicy"
+AmazonEKSLoad = "arn:aws:iam::aws/AmazonEKSLoadBalancingPolicy"
+}
+}
 
       /* taints = {
         upgrade = {
