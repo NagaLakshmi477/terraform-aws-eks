f59a534adbee33bf1c7ee65ceafd2e82373b8d2e
NagaLakshmi477 <jaishikareddy997@gmail.com>
2026-08-04 10:17:49 +0530

    k8s

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -5,7 +5,7 @@ module "vpc" {
   public_subnet_cidr   = var.pub_subnet
   private_subnet_cidr  = var.pri_subnet
   database_subnet_cidr = var.db_subnet
- # is_peering_required = true
+  is_peering_required = true
 }
 
 # here we have all code in source right so we need to give module and modulename
